3d33d99814dd6541d8c9b6d232e5ff22d250de14
jaikrishnaverma <jaikrishnaverma>
2022-10-13 17:19:38 +0530

    task1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <img src="Desktop.png" />
+      <img src="Desktop.png" /> */}
+     <img src='Desktop - 1.png' />
+    </div>
+  );
+}
+
+export default App;
